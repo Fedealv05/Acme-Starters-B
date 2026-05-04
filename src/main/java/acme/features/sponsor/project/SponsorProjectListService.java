813f7629a883fbd36ce007adc6fd2fdd01c863c2
fedealv_05 <fedealvarezgarcia12@gmail.com>
2026-05-04 16:15:27 +0200

    feat: sponsor puede listar y detallar proyectos

---
+++ src/main/java/acme/features/sponsor/project/SponsorProjectListService.java
@@ -0,0 +1,41 @@
+
+package acme.features.sponsor.project;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.services.AbstractService;
+import acme.entities.projects.Project;
+import acme.realms.Sponsor;
+
+@Service
+public class SponsorProjectListService extends AbstractService<Sponsor, Project> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SponsorProjectRepository	repository;
+
+	private Collection<Project>			projects;
+
+
+	@Override
+	public void load() {
+
+		this.projects = this.repository.findPublishedProjects();
+	}
+
+	@Override
+	public void authorise() {
+
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObjects(this.projects, "title", "keyWords", "description", "kickOff", "closeOut", "draftMode");
+	}
+
+}
